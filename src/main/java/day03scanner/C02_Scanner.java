25322307c51c6b67affe95fa12a933bc6629ed64
Fatih Deniz <deniz.fatih@hotmail.de>
2022-12-01 10:11:26 +0100

    Day 03

--- src/main/java/day03scanner/C02_Scanner.java
+++ src/main/java/day03scanner/C02_Scanner.java
@@ -1,5 +1,52 @@
 package day03scanner;
 
+import java.util.Scanner;
+
 public class C02_Scanner {
     //Scanner class'i kullanici ile etkilesim kurmamizi saglar
+    //Scanner bir class'tir. Java util kütüphanesinden getirildigi icin import ister
+
+    public static void main(String[] args) {
+
+        //Kullanicidan Data almak
+        //1.Adim Scanner class'indan bir Object olustur
+        Scanner input = new Scanner(System.in);
+
+
+        //2.Adim Kullanicinin ne yapacagini söyle
+        System.out.println("Lütfen yasinizi giriniz");
+
+        //3.Adim Kullanicidan aldiginiz datayi variable icine koyun
+        byte age = input.nextByte();
+
+
+
+        //Kullanici ad,memleket, yas, boy, yasadigi yeri sevip sevmedigini soran program
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Lütfen isminizi giriniz ...:");
+        String isim = scan.nextLine();
+
+        System.out.println("Lütfen memleketinizi giriniz ...: ");
+        String memleket = scan.nextLine();
+
+        System.out.println("Lütfen yasinizi giriniz ...:");
+        byte yas = scan.nextByte();
+
+        System.out.println("Lütfen boyunuzu giriniz ...:");
+        short boy=scan.nextShort();
+
+        System.out.println("Lütfen yasadiginiz yeri sevip sevmediginizi yaziniz ...:");
+        boolean seviyorMu = scan.nextBoolean();
+
+        System.out.println("****************************");
+
+        System.out.println("isim = " + isim);
+        System.out.println("memleket = " + memleket);
+        System.out.println("yas = " + yas);
+        System.out.println("boy = " + boy);
+        System.out.println("seviyorMu = " + seviyorMu);
+
+
+    }
 }
---
+++ src/main/java/day03scanner/C03_Scanner.java
@@ -0,0 +1,8 @@
+package day03scanner;
+
+public class C03_Scanner {
+
+    public static void main(String[] args) {
+
+    }
+}
