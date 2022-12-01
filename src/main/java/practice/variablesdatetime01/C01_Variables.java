94d808cab76857569b445fcb641b20f3d85162a9
Fatih Deniz <deniz.fatih@hotmail.de>
2022-12-01 08:50:43 +0100

    first version

--- src/main/java/practice/variablesdatetime01/C01_Variables.java
+++ src/main/java/practice/variablesdatetime01/C01_Variables.java
@@ -44,5 +44,66 @@ public class C01_Variables {
         double salary = 20;
 
         System.out.println("The total fee is calculated as :" + period * salary + "$");
+
+
+
+        //1)TECHPRO EDUCATION ==> her bir harf alt alta gelecek sekilde yazdiran kodu olusturunuz.
+
+        System.out.println("*************1. YOL**********");
+        System.out.println("T");
+        System.out.println("E");
+        System.out.println("C");
+        System.out.println("H");
+        System.out.println("P");
+        System.out.println("R");
+        System.out.println("O");
+        System.out.println("");//HICLIKTE OLUR
+        System.out.println("E");
+        System.out.println("D");
+        System.out.println("U");
+        System.out.println("C");
+        System.out.println("A");
+        System.out.println("T");
+        System.out.println("I");
+        System.out.println("O");
+        System.out.println("N");
+        /*
+        \n ==> alt satira gecirir
+        \t ==> bir tab bosluk birakir
+        \b ==> gerisindeki ilk harfi siler
+        ctrl+ alt +l ==> windows icin sayfayi duzenler
+        cmd + alt +l ==> mac icin sayfayi duzenler
+         */
+
+        //2)yukaridaki ornegi tek sout ile her harf alt alta gelecek sekilde yazdirin
+        System.out.println("************2.Yol******************");
+        System.out.println("\nT\nE\nC\nH\nP\nR\nO\n\nE\nD\nU\nC\nA\nT\nI\nO\nN");
+
+        //3) JAVA ILE GUZEL DUNYA her bir kelimeyi alt satira yazdiracak sekilde kodu create ediniz
+
+        System.out.println("\nJAVA\nILE\nGUZEL\nDUNYA");
+        System.out.println("JAVA");
+        System.out.println("ILE");
+        System.out.println("GUZEL");
+        System.out.println("DUNYA");
+
+        //4) JAVA ILE GUZEL DUNYA seklinde kelimeler arasinda 1 tab bosluk birakan kodu yaziniz
+        System.out.println("JAVA\tILE\tGUZEL\tDUNYA");
+
+        //5) pazartesi kelimesinin her bir harfini alt satirda yazdiracak sekilde tek sout ile kodu create ediniz
+        System.out.println("\np\na\nz\na\nr\nt\ne\ns\ni");
+        //Soru-6 : "Techpro" ile java cok 'kolay'
+        // yazdiran bir kod yaziniz
+
+        System.out.println("\"Techpro\"\tile\tjava\tcok\t'kolay'");
+        //Soru-7 : "MAHARET" hic 'DUSMEMEK'  degil;
+
+        // "Her dustugunde kalkabilmektir" ==>ifadesini
+
+        //      seklinde noktali virgulden
+        //      sonra 3 satir asagidan ve satir basi yaparak
+        //      yazdiran bir kod yaziniz.
+       System.out.println("\"MAHARET\" hic\t'DUSMEMEK'\tdegil;\n\n\n\t\"Her dustugunde kalkabilmektir\"");
+
     }
 }
