8645c07ae2c51e8e95fc8c06b5e33eeff35ecf77
Fatih Deniz <deniz.fatih@hotmail.de>
2022-12-01 10:19:44 +0100

    Day 03

--- src/main/java/day03scanner/C03_Scanner.java
+++ src/main/java/day03scanner/C03_Scanner.java
@@ -1,8 +1,22 @@
 package day03scanner;
 
+import java.util.Scanner;
+
 public class C03_Scanner {
 
     public static void main(String[] args) {
+
+        //EX 1) Kullanicidan sayilari alarak toplama islemi yaptiran kodu yaziniz
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Lütfen birinci sayiyi giriniz");
+        int sayi1 = input.nextInt();
+
+        System.out.println("Lütfen ikinci sayiyi giriniz");
+        int sayi2 = input.nextInt();
+
+        System.out.println("Toplam : " + (sayi1 + sayi2));
 
     }
 }
