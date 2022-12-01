dd5f6589026d230455b8e3e4d9a817601e117a68
Fatih Deniz <deniz.fatih@hotmail.de>
2022-12-01 09:42:24 +0100

    Day 03

---
+++ src/main/java/day03scanner/C02_Scanner.java
@@ -0,0 +1,5 @@
+package day03scanner;
+
+public class C02_Scanner {
+    //Scanner class'i kullanici ile etkilesim kurmamizi saglar
+}
---
+++ src/main/java/day03scanner/Car.java
@@ -0,0 +1,30 @@
+package day03scanner;
+
+public class Car {
+
+    //Class ==> Variable (Pasif özellikler)
+    //Method (Aktif özellikler)
+
+
+    public String model = "Corolla"; //Pasif özellikler
+    public int fiyat = 200000;
+
+    public static void main(String[] args) {
+        //Class_adi   Obje_Adi  = new keywordu  Class_Adi();
+        Car myCorolla = new Car();
+        System.out.println("Fiyati: " + myCorolla.fiyat +"TL");
+        System.out.println("Modeli: " + myCorolla.model);
+
+        myCorolla.hareket();
+        myCorolla.dur();
+
+    }//Main body'si
+
+    public void hareket() { //Aktif özellikler
+        System.out.println("Toyota hizli hareket eder...");
+    }
+
+    public void dur() {
+        System.out.println("Toyota güvenli durur...");
+    }
+} //Class body'si
