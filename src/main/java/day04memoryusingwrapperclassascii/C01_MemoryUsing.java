6206ce46ad17c5b0fbe115a87af1040f7b1244e3
Fatih Deniz <deniz.fatih@hotmail.de>
2022-12-02 09:33:40 +0100

    Day 04

---
+++ src/main/java/day04memoryusingwrapperclassascii/C01_MemoryUsing.java
@@ -0,0 +1,15 @@
+package day04memoryusingwrapperclassascii;
+
+public class C01_MemoryUsing {
+
+    /*
+        Javanin kullandigi ve ürettigi hersey Data'dir
+        Java'da iki türlü memory(hafiza) vardir.
+        1) Stack (small) memory : Primitive data type'lar ve Non-Primitive data type'lerin referanslarini tutar
+        2) Heap (huge) memory : Non-Primitive data type'lerin depolandigi alandir.
+
+        Note: Java, Heap Memory'e yerlestiren tüm Non-Primitve data'lar icin Stack Memory'de bir adres (Referans) saklar.
+
+        Note: Heap 'de kullanilmayan datalarin temizligini Garbage Collector (Cöp toplayicisi) yapar.
+     */
+}
