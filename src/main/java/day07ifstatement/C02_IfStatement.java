189689e05919e85aa2425fbf7c2b994ea50198ec
Fatih Deniz <deniz.fatih@hotmail.de>
2022-12-08 11:20:58 +0100

    Day07

--- src/main/java/day07ifstatement/C02_IfStatement.java
+++ src/main/java/day07ifstatement/C02_IfStatement.java
@@ -11,5 +11,27 @@ public class C02_IfStatement {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
+        System.out.println("Lütfen gün numarasini giriniz..\n1: Pazar\t2: Pazartesi\t3: Sali\t4: Carsamba\t5: Persembe\t6: Cuma\t7: Cumartesi ");
+
+        byte dayNummber = input.nextByte();
+
+        if (dayNummber == 1) {
+            System.out.println("Pazar");
+        } else if (dayNummber == 2) {
+            System.out.println("Pazartesi");
+        } else if (dayNummber == 3) {
+            System.out.println("Sali");
+        } else if (dayNummber == 4) {
+            System.out.println("Carsamba");
+        } else if (dayNummber == 5) {
+            System.out.println("Persembe");
+        } else if (dayNummber == 6) {
+            System.out.println("Cuma");
+        } else if (dayNummber == 7) {
+            System.out.println("Cumartesi");
+        } else
+            System.out.println("Lütfen Gecerli Bir Gün Numarasi Giriniz!");
+
+
     }
 }
