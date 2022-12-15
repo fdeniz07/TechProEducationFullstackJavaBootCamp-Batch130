7024b059f3bc20ea0868603c46ec9ad32a37f5dc
Fatih Deniz <deniz.fatih@hotmail.de>
2022-12-15 12:08:03 +0100

    day 11

--- src/main/java/day11loops/C07_NestedLoop.java
+++ src/main/java/day11loops/C07_NestedLoop.java
@@ -1,4 +1,32 @@
 package day11loops;
 
+import java.util.Scanner;
+
 public class C07_NestedLoop {
+    public static void main(String[] args) {
+
+        /*
+           Type code to print the following image on the console
+                X X X X X
+                X X X X X
+                X X X X X
+        */
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Lütfen satir bilgisini giriniz: ");
+        int row = input.nextInt();
+        System.out.print("Lütfen sütun bilgisini giriniz: ");
+        int column = input.nextInt();
+        System.out.print("Lütfen cizilmesini istediginiz karakteri giriniz: ");
+        char character = input.next().charAt(0);
+
+        for (int i = 1; i < row + 1; i++) {
+            for (int j = 1; j < column + 1; j++) {
+                System.out.print(" " + character + " ");
+            }
+            System.out.println();
+        }
+
+
+    }
 }
