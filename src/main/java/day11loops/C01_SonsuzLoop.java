d6d088891cce8bece1538821e39bc80af065a7f4
Fatih Deniz <deniz.fatih@hotmail.de>
2022-12-15 09:06:54 +0100

    day 11

---
+++ src/main/java/day11loops/C01_SonsuzLoop.java
@@ -0,0 +1,12 @@
+package day11loops;
+
+public class C01_SonsuzLoop {
+    public static void main(String[] args) {
+
+        for (int i=0; i>=0; i++){
+
+            //Infinite Loop
+        }
+
+    }
+}
