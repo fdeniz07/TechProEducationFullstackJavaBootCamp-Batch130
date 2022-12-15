78a4d9f4a95828cd9e8a8a0c71b11c7f0c607b51
Fatih Deniz <deniz.fatih@hotmail.de>
2022-12-16 00:23:15 +0100

    day 11

--- src/main/java/day11loops/C03_ForLoop.java
+++ src/main/java/day11loops/C03_ForLoop.java
@@ -6,8 +6,8 @@ public class C03_ForLoop {
 
     public static void main(String[] args) {
 
-        /*  String karakterleri teker teker yazdiran;
-            Eger 'a' karakterini görürse yazdirmayi durduran kodu yaziniz
+        /* Example 1: String karakterleri teker teker yazdiran;
+                      Eger 'a' karakterini görürse yazdirmayi durduran kodu yaziniz
          */
 
         String word = "Ögrenmek yasamin tek kanitidir";
@@ -20,5 +20,37 @@ public class C03_ForLoop {
             }
             System.out.print(word.charAt(i) + "-");
         }
+
+
+        //Example 2: Verilen bir String'de ilk 'a' harfinden onceki tum character'leri console'a yazdiriniz
+        //           "I love Java" ==> "I love J"
+        String s = "Tramvay";
+
+        for(int i=0; i<s.length(); i++){
+
+            char ch = s.charAt(i);
+
+            if(ch=='a'){
+                break;
+            }
+            System.out.print(ch);
+        }
+
+        System.out.println();
+
+        //Example 3: Verilen bir String'de son 'a' dan sonraki tum character'leri ters sirada yazdiriniz
+        //           "Germany" ==> yn
+        String t = "Germany";
+
+        for(int i=t.length()-1; i>=0; i--){
+            char ch = t.charAt(i);
+            if(ch=='a'){
+                break;
+            }
+            System.out.print(ch);
+        }
+
+
+
     }
 }
