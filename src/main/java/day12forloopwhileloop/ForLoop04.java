2b44579dc73e337dd2253665250a7df4aebf627e
Fatih Deniz <deniz.fatih@hotmail.de>
2022-12-16 21:09:20 +0100

    day 12

--- src/main/java/day12forloopwhileloop/ForLoop04.java
+++ src/main/java/day12forloopwhileloop/ForLoop04.java
@@ -1,4 +1,35 @@
 package day12forloopwhileloop;
 
+import java.util.Scanner;
+
 public class ForLoop04 {
+    public static void main(String[] args) {
+
+        /*
+           Example 1:
+           Kullanicidan baslangic ve bitis degerlerini alin.
+           Baslangic degerinden baslayip bitis degerine kadar aradaki tum cift tamsayilari
+           ekrana yazdiriniz  //10  12  14
+        */
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Baslangic degerini giriniz : ");
+        int start = input.nextInt();
+
+        System.out.print("Bitis degerini giriniz : ");
+        int end = input.nextInt();
+
+
+        if (start>end){
+            System.out.println("Baslangic degeri bitis degerinden buyuk olamaz...");
+        }
+        else {
+            for (int i = start; i <= end; i++) {
+                if (i % 2 == 0) {
+                    System.out.print(i + " ");
+                }
+            }
+        }
+    }
 }
