93d0fff002bc897d841f56fb522b7e8ede8d0658
Fatih Deniz <deniz.fatih@hotmail.de>
2022-12-16 19:38:57 +0100

    day 12

---
+++ src/main/java/day12forloopwhileloop/C02_ForLoopSayiKullanmadanJava.java
@@ -0,0 +1,12 @@
+package day12forloopwhileloop;
+
+public class C02_ForLoopSayiKullanmadanJava {
+    public static void main(String[] args) {
+        /*
+        1'den 100'e kadar hic sayi kullanmadan console yazdiriniz
+         */
+        for (int i = 'e' / 'e'; i < 'e'; i++) {//e 'nin Ascci degeri 101 'd'=100
+            System.out.print(i + " ");
+        }
+    }
+}
