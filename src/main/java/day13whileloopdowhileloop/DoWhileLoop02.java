d914baa16c48514a7fcbbb018c87b74a06011e39
Fatih Deniz <deniz.fatih@hotmail.de>
2022-12-17 21:10:40 +0100

    Day 13

--- src/main/java/day13whileloopdowhileloop/DoWhileLoop02.java
+++ src/main/java/day13whileloopdowhileloop/DoWhileLoop02.java
@@ -1,4 +1,31 @@
 package day13whileloopdowhileloop;
 
+import java.util.Scanner;
+
 public class DoWhileLoop02 {
+    public static void main(String[] args) {
+
+        /*
+             Kullanicidan bir tam sayi aliniz
+             Tam sayi 100'den kucuk ise kullaniciya "Kaybettiniz!" mesaji vererek oyunu sonlandiriniz
+             Tam sayi 100 ve 100'den büyük ise "Kazandiniz!" mesaji vererek oyuna devam ettiriniz.
+        */
+
+        Scanner input = new Scanner(System.in);
+
+        int num = 0;
+
+        do {
+            System.out.print("Bir sayi giriniz: ");
+            num = input.nextInt();
+
+            if (num >= 100) {
+                System.out.println("Kazandiniz!");
+            }
+
+        } while (num >= 100);
+
+        System.out.println("Kaybettiniz");
+
+    }
 }
---
+++ src/main/java/day13whileloopdowhileloop/DoWhileLoop03.java
@@ -0,0 +1,4 @@
+package day13whileloopdowhileloop;
+
+public class DoWhileLoop03 {
+}
