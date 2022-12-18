89334e8a2bfbbb7c3d01be975342706734e10934
Fatih Deniz <deniz.fatih@hotmail.de>
2022-12-18 18:13:44 +0100

    SSG - String Manipulation

--- src/main/java/SSG/StringManipulation05.java
+++ src/main/java/SSG/StringManipulation05.java
@@ -2,4 +2,17 @@ package SSG;
 
 public class StringManipulation05 {
 
+    /*
+      soru 5)Bir String değişkeni oluşturunuz ve String değişkenin konsolda boşluk olmayan son
+             karakteri yazdırınız.
+  */
+    public static void main(String[] args) {
+
+
+        String str = "java kolaydır   ";
+        int sonKarakterinIndexi = str.trim().length() - 1;
+        String sonkarakter = str.substring(sonKarakterinIndexi, sonKarakterinIndexi + 1);
+        System.out.println(sonkarakter);
+
+    }
 }
