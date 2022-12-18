62e6f244110b92ca1acfd974bc88556aea75f492
Fatih Deniz <deniz.fatih@hotmail.de>
2022-12-18 18:13:02 +0100

    SSG - String Manipulation

--- src/main/java/SSG/StringManipulation04.java
+++ src/main/java/SSG/StringManipulation04.java
@@ -1,7 +1,18 @@
 package SSG;
 
 public class StringManipulation04 {
-    /*
-        Soru4)
-     */
+    /*soru 4)Kişi isimleri için 3 String değişken oluşturunuz. Boşluk karakterleri hariç 3 isimdeki
+    karakter sayısının toplamını yazdırınız.
+    */
+    public static void main(String[] args) {
+
+        String isim1 = "ahmet furkan     ";
+        String isim2 = "utku can";
+        String isim3 = "mehmet ali";
+
+        int a1 = isim1.replaceAll("\\s", "").length();
+        int a2 = isim2.replaceAll("\\s", "").length();
+        int a3 = isim3.replaceAll("\\s", "").length();
+        System.out.println("karakterlerin toplamı : " + (a1 + a2 + a3));
+    }
 }
