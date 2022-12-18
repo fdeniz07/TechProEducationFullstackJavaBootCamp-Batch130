c08727458c46da2c363e70d3e5ce67b09a0b3055
Fatih Deniz <deniz.fatih@hotmail.de>
2022-12-18 18:14:14 +0100

    SSG - String Manipulation

--- src/main/java/SSG/StringManipulation06.java
+++ src/main/java/SSG/StringManipulation06.java
@@ -1,5 +1,16 @@
 package SSG;
 
 public class StringManipulation06 {
+    public static void main(String[] args) {
+        /*
+        soru 6) Sadece tek bir kelimeden oluşan şehir isimleri için bir String değişkeni oluşturun. Şehir
+        ismini, baş harfi büyük ve diğer tüm karakterleri küçük harfler olacak şekilde konsolda
+        yazdırıniz.
+        */
+        String sehirIsmi = "  kAYSERI";
+        String str = sehirIsmi.trim().toLowerCase();
+        str = str.substring(0, 1).toUpperCase() + str.substring(1);
+        System.out.println(str);
 
+    }
 }
