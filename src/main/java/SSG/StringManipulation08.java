207e3ed402f0771de1fb463b7c015730178e3ba4
Fatih Deniz <deniz.fatih@hotmail.de>
2022-12-18 18:15:16 +0100

    SSG - String Manipulation

--- src/main/java/SSG/StringManipulation08.java
+++ src/main/java/SSG/StringManipulation08.java
@@ -1,4 +1,17 @@
 package SSG;
 
 public class StringManipulation08 {
+    /*
+    Bir String’ in başında büyük harf ve sonunda nokta olup olmadığını kontrol etmek için kod
+    yazınız
+     */
+    public static void main(String[] args) {
+        String str = "Ömer faruk.";
+        char ilkkaraker = str.charAt(0);
+        char sonkarakter = str.charAt(str.length() - 1);
+        boolean ilkkarakerbuyukmu = ilkkaraker >= 'A' && ilkkaraker <= 'Z';
+        boolean sonkarakternoktami = sonkarakter == '.';
+        boolean ikisinikontrol = ilkkarakerbuyukmu && sonkarakternoktami;
+        System.out.println(ikisinikontrol);
+    }
 }
