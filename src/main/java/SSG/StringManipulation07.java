e0bd31bf0eaeddff37616dc7e7c60444cbd55402
Fatih Deniz <deniz.fatih@hotmail.de>
2022-12-18 18:14:42 +0100

    SSG - String Manipulation

--- src/main/java/SSG/StringManipulation07.java
+++ src/main/java/SSG/StringManipulation07.java
@@ -1,4 +1,12 @@
 package SSG;
 
 public class StringManipulation07 {
+    /*
+    girilen Stringdeki tum rakamları "*" ile degistirin
+    */
+    public static void main(String[] args) {
+        String str = "bu4ak1";
+        String a = str.replaceAll("[0-9]", "*");
+        System.out.println(a);
+    }
 }
