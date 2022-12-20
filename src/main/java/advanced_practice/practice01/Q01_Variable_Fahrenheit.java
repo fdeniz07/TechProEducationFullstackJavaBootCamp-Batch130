a68454f010eb06ac62d5e266db643859cbc8ce1d
Fatih Deniz <deniz.fatih@hotmail.de>
2022-12-20 10:06:59 +0100

    Advanced Practice 01

---
+++ src/main/java/advanced_practice/practice01/Q01_Variable_Fahrenheit.java
@@ -0,0 +1,22 @@
+package advanced_practice.practice01;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+public class Q01_Variable_Fahrenheit {
+    // Fahrenheit değeri, Celsius değere çeviren kod yazınız.
+    // formül: c = (f-32)*5/9
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Fahrenheit degeri giriniz.");
+        double f = input.nextDouble();
+        double c = (f - 32) * 5 / 9;
+        System.out.println("Celcius: " + c);
+
+        DecimalFormat format = new DecimalFormat("0.00");
+        String formattedCelcius = format.format(c);
+        System.out.println("formattedCelcius = " + formattedCelcius);
+
+        System.out.printf("Formatli:  " + "%.2f",c);
+    }
+}
