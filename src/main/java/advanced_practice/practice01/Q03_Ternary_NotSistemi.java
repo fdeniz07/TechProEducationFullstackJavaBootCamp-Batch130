60af6631d4d917ee188a3d4d87da6c976116bbe4
Fatih Deniz <deniz.fatih@hotmail.de>
2022-12-20 10:11:00 +0100

    Advanced Practice 01

---
+++ src/main/java/advanced_practice/practice01/Q03_Ternary_NotSistemi.java
@@ -0,0 +1,27 @@
+package advanced_practice.practice01;
+
+import java.util.Scanner;
+
+public class Q03_Ternary_NotSistemi {
+    /*
+  Yüzlük not cinsinden kullanıcıdan alınan notu harf not sistemine çeviren bir kod yazınız.(Ternary kullanınız)
+  (90 ve üstü A, 80-89 B, 70-79 C, 60-69 D, 50-59 E, Diğerleri F)
+   */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Notunuzu sayi cinsinden giriniz.");
+        int notSayi = input.nextInt();
+
+        String notHarf = notSayi > 100 ?
+                "0 ile 100 arasi bir not giriniz" : notSayi > 89 ?
+                "A" : notSayi > 79 ?
+                "B" : notSayi > 69 ?
+                "C" : notSayi > 59 ?
+                "D" : notSayi > 49 ?
+                "E" : notSayi > -1 ? "F" : "0 ile 100 arasi bir not giriniz";
+
+        System.out.println("notHarf = " + notHarf);
+
+
+    }
+}
