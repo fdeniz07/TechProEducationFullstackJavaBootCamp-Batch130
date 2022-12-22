c49c3778019a4e53b2f3829de8c73405402552fe
Fatih Deniz <deniz.fatih@hotmail.de>
2022-12-22 12:31:44 +0100

    Day 15

---
+++ src/main/java/day15arraysmultidimensionalarrays/C04_Arrays.java
@@ -0,0 +1,23 @@
+package day15arraysmultidimensionalarrays;
+
+public class C04_Arrays {
+    // Bir Arrayin icinde herhangi bir elemanin olup olmadigini
+    // kontrol eden ve kac kere tekrarlandigini gosteren kodu yaziniz
+
+    public static void main(String[] args) {
+        int arr[] = {5, 1, 5, -3};
+        int eleman = -90;
+        int sayac = 0;
+
+        for (int w : arr) {
+            if (w == eleman) {
+                sayac++;
+            }//if
+        }//for
+
+        if (sayac > 0) {
+            System.out.println(eleman + " Array'de " + sayac + " defa var");
+        } else
+            System.out.println(eleman + " Array'de yok");
+    }
+}
