db8de49b904599508e620cc5cb7c8ff1f84afac4
Fatih Deniz <deniz.fatih@hotmail.de>
2022-12-24 19:13:16 +0100

    Practice 05

---
+++ src/main/java/practice/practiceDTNT/C04_ArraysKarakterleriSaydir.java
@@ -0,0 +1,12 @@
+package practice.practiceDTNT;
+
+public class C04_ArraysKarakterleriSaydir {
+     /*
+    INTERWIEW QUESTIONS
+     Kullanicidan bir String aliniz.
+     String'de var olan her character'in sayisini ekrana yazdiriniz.
+     Ornek: alacan ==> a = 3, l = 1, c = 1, n = 1
+            abaa   ==> a=3  b=1
+    */
+    // O    D   E   V  \\
+}
