b7445322a5d23a825da884e065b9726fec1a951c
Fatih Deniz <deniz.fatih@hotmail.de>
2022-12-24 21:09:11 +0100

    day15

--- src/main/java/day17arraylists/ArrayLists01.java
+++ src/main/java/day17arraylists/ArrayLists01.java
@@ -1,6 +1,8 @@
 package day17arraylists;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 public class ArrayLists01 {
         /*    Array ile ArrayList'in farki nedir?
@@ -33,16 +35,69 @@ public class ArrayLists01 {
         System.out.println(ages); //[12, 9, 10]
 
         //Eger bir degeri, istedigimiz indexe eklemek istersek; add metodu icinde önce index numarasi sonra da deger verilir.
-        ages.add(1,656);
+        ages.add(1, 656);
         System.out.println(ages); //[12,656, 9, 10]
 
-        ages.add(3,777);
+        ages.add(3, 777);
         System.out.println(ages); //[12, 656, 9, 777, 10]
 
         ages.add(888);
         System.out.println(ages); //[12, 656, 9, 777, 10]
+
+        //Array-List'lere nasil coklu eleman eklenir? veya List'e baska bir List nasil eklenir?
+        //Bir "List"e coklu eleman eklemek, o elemanlari önce bir List'in icine koymalisiniz
+        ArrayList<Integer> newAges = new ArrayList<>();
+        newAges.add(8);
+        newAges.add(9);
+        newAges.add(10);
+
+        ages.addAll(newAges);//[9, 656, 12, 777, 10, 888, 8, 9, 10]
+        //Eger bir list'i, istedigimiz indexe eklemek istersek; addAll metodu icinde önce index numarasi sonra da List verilir.
+        ages.addAll(2, newAges);//[9, 656, 8, 9, 10, 12, 777, 10, 888, 8, 9, 10]
+        System.out.println(ages);//[9, 656, 12, 777, 10, 888, 8, 9, 10]
+
+        //Bir List'teki tum elemanlari nasil silebilirim?
+        //ages.clear();
+        System.out.println(ages);
+
+        //toArray() metodu: eleman sayisini degitirmeyecegimizden eminsek array' ye cevirip memory kullnimini azaltabiliriz
+
+        //contains() metodu ilgili eleman list icinde var mi diye kontrol eder
+        boolean r = ages.contains(656);
+        System.out.println(r);
+
+        //Bir List'in baska bir List ile ayni olup olmadigini nasil kontrol ederiz?
+        ArrayList<String> names1 = new ArrayList<>();
+        names1.add("Tom");
+        names1.add("Jim");
+        names1.add("Kim");
+
+        ArrayList<String> names2 = new ArrayList<>();
+        names2.add("Tom");
+        names2.add("Kim");
+        names2.add("Jim");
+
+        //Note: Iki List'in esit olabilmesi icin ayni indexte ayni elemanlar olmalidir.
+        boolean s = names1.equals(names2);
+        System.out.println(s); //false
+
+        //Example 1: Verilen iki integer List'te tamamiyle ayni elemanlarin olup olmadigini kontrol eden kodu yaziniz.
+
+        ArrayList<Integer> nums1 = new ArrayList<>();
+        nums1.add(8);
+        nums1.add(7);
+        nums1.add(9);
+
+        ArrayList<Integer> nums2 = new ArrayList<>();
+        nums2.add(8);
+        nums2.add(9);
+        nums2.add(10);
+
+        Collections.sort(nums1);
+        Collections.sort(nums2);
+
+        boolean t = nums1.equals(nums2);
+        System.out.println(t);
+
     }
-
-
-
 }
