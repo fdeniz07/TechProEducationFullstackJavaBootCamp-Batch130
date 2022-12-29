f4b6d7758e309ce91f442068271f262579bbdab7
Fatih Deniz <deniz.fatih@hotmail.de>
2022-12-29 11:55:28 +0100

    Day 18

--- src/main/java/day18arraylists/ArrayLists02.java
+++ src/main/java/day18arraylists/ArrayLists02.java
@@ -2,6 +2,7 @@ package day18arraylists;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class ArrayLists02 {
 
@@ -28,5 +29,36 @@ public class ArrayLists02 {
             }
         }
         System.out.println("newList = " + newList); //newList = [2, 3, 5]
+
+        //Example 2: Musterinizin istedigi urunun stoklarinizda olup olmadigini gosteren kodu yaziniz.
+        //Stokta varsa o urunu "Ayrildi" ya ceviriniz yoksa o urunu  List'e ekleyiniz
+        //           [A, K, R, S]  ==> R  ==> [A, K, Ayrildi, S]
+        //           [A, K, R, S]  ==> X  ==> [A, K, R, S, X]
+
+        Scanner input = new Scanner(System.in);
+        List<String> c = new ArrayList<>();
+        c.add("A");
+        c.add("K");
+        c.add("R");
+        c.add("S");
+
+        String urun = "";
+
+
+        do {
+            System.out.println("Lutfen istediginiz urunun harfini giriniz");
+            urun = input.next().substring(0, 1);
+
+            if (c.contains(urun)) {
+                c.set(c.indexOf(urun), "Ayrildi");
+
+            } else if (!urun.equals("Q")) {
+                c.add(urun);
+            }
+            System.out.println(c);
+
+        } while (!urun.equals("Q"));
+
+
     }
 }
