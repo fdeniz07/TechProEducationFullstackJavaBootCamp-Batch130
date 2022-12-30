b04811bfe6668e7d4030cce354b96d3c84469b66
Fatih Deniz <deniz.fatih@hotmail.de>
2022-12-30 09:31:27 +0100

    SSG

---
+++ src/main/java/ssg/forloops/C03_ForLoops.java
@@ -0,0 +1,23 @@
+package ssg.forloops;
+
+public class C03_ForLoops {
+
+    public static void main(String[] args) {
+ /*
+ soru 3) 100'e(dahil) kadar olan tek ve cift sayıları iki ayrı dongu ile yazdırın.
+ */
+        for (int i = 0; i <= 100; i++) {
+            if (i % 2 == 1) {
+                System.out.print(i + " ");
+            } else System.out.println(i);
+
+        }
+        System.out.println("\n");
+        for (int i = 0; i <= 100; i++) {
+            if (i % 2 == 0) {
+                System.out.print(i + " ");
+            }
+
+        }
+    }
+}
