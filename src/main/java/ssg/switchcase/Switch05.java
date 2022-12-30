858f8534f91ed955a9e6d323aa7b565b7f259087
Fatih Deniz <deniz.fatih@hotmail.de>
2022-12-30 19:24:26 +0100

    SSG

--- src/main/java/ssg/switchcase/Switch05.java
+++ src/main/java/ssg/switchcase/Switch05.java
@@ -1,4 +1,36 @@
 package ssg.switchcase;
 
+import java.util.Scanner;
+
 public class Switch05 {
+
+    public static void main(String[] args) {
+        //ingilizce de Ünlü veya Ünsüz harflerini kontrol etmek için bit program yaziniz:
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("lutfen bir ingilizce harfi yaziniz");
+        char ch=input.next().toLowerCase().charAt(0);
+        switch(ch) {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+                System.out.println("=="  + ch + "=="  + " vowela bir harf dir : ");
+                break;
+
+                /*
+                    case 'A':ß
+                    case 'E':
+                    case 'I':
+                    case 'O':
+                    case 'U':
+                System.out.println("=="  + ch + "=="  + " vowela bir harf dir : ");
+                break;
+                 */
+
+            default:
+                System.out.println("-"  + ch + "-"  + " Consonant bir harf dir : ");
+        }
+    }
 }
