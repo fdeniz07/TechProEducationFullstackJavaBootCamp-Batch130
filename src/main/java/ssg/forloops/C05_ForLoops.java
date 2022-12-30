08c2cd30ff85511ae0faf66a70782caeebfaf1b5
Fatih Deniz <deniz.fatih@hotmail.de>
2022-12-30 09:33:28 +0100

    SSG

---
+++ src/main/java/ssg/forloops/C05_ForLoops.java
@@ -0,0 +1,21 @@
+package ssg.forloops;
+
+import java.util.Scanner;
+
+public class C05_ForLoops {
+
+    public static void main(String[] args) {
+        /*
+        Soru 5) Kullanicidan 100'den kucuk bir tamsayi isteyin. 1'den baslayarak girilen sayiya kadar 3'un kati olan sayilari yazdirin.
+         */
+        Scanner scan = new Scanner(System.in);
+        System.out.println("lutfen 100den kucuk bir sayi giriniz");
+        int sayi = scan.nextInt();
+        for (int i = 1; i <= sayi; i++) {
+            if (i % 3 == 0) {
+                System.out.print(i + " ");
+            }
+
+        }
+    }
+}
