e1ca469626ca09e61d1878bbaf5512eb923a7546
Fatih Deniz <deniz.fatih@hotmail.de>
2022-12-30 18:12:36 +0100

    SSG

---
+++ src/main/java/ssg/forloops/C12_ForLoops.java
@@ -0,0 +1,28 @@
+package ssg.forloops;
+
+public class C12_ForLoops {
+
+     /*
+        Aşağıdaki görüntüyü bir for döngüsü kullanarak elde eden kodu yazınız
+        * * * * * *
+        * * * * *
+        * * * *
+        * * *
+        * *
+        *
+    */
+
+
+    public static void main(String[] args) {
+        int sayi = 6;
+
+        for (int i = 1; i <= sayi; i++) {
+            for (int j = sayi; j >= i; j--) {
+                System.out.print("* ");
+
+            }
+            System.out.println();
+
+        }
+    }
+}
