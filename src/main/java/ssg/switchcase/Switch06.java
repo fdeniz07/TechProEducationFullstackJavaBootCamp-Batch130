e15a6018d0805f4508c1021f60367e66eaa792c1
Fatih Deniz <deniz.fatih@hotmail.de>
2022-12-30 19:25:10 +0100

    SSG

---
+++ src/main/java/ssg/switchcase/Switch06.java
@@ -0,0 +1,42 @@
+package ssg.switchcase;
+
+import java.util.Scanner;
+
+public class Switch06 {
+
+    // Kullanicidan kacinci ay oldugunu alip mevsimi yazdiran bir
+    // switch olusturun
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Lutfen kacinci ay oldugunu giriniz");
+        String ayIsmi=scan.next();
+        switch (ayIsmi){
+
+            case "Mart":
+            case "Nisan":
+            case "Mayis":
+                System.out.println("Ilkbahar");
+                break;
+            case "Haziran":
+            case "Temmuz":
+            case "Agustos":
+                System.out.println("Yaz");
+                break;
+            case "Eylul":
+            case "Ekim":
+            case "Kasim":
+                System.out.println("Sonbahar");
+                break;
+            case "Aralik":
+            case "Ocak":
+            case "Subat":
+                System.out.println("Kis");
+                break;
+
+            default:
+                System.out.println("Lutfen gecerli bir ay Ismi giriniz");
+        }
+    }
+
+}
