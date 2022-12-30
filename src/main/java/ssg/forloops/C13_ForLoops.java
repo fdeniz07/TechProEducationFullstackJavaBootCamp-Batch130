18b4e5cf86ac8282a02bc867a2dec2989c9daf9b
Fatih Deniz <deniz.fatih@hotmail.de>
2022-12-30 18:41:32 +0100

    SSG

---
+++ src/main/java/ssg/forloops/C13_ForLoops.java
@@ -0,0 +1,18 @@
+package ssg.forloops;
+
+public class C13_ForLoops {
+    public static void main(String[] args) {
+        /*
+        Bir String’ de benzersiz(tekrarsız) karakterler yazdırmak için kod yazınız.
+        */
+
+        String str = "Merhaba";
+        String str1 = "";
+        for (int i = 0; i < str.length(); i++) {
+            str1 = str.substring(i, i + 1);
+            if (str.indexOf(str1) == str.lastIndexOf(str1)) {
+                System.out.print(str1 + " ");
+            }
+        }
+    }
+}
