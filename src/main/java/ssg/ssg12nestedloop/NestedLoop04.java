e953e98b0404e994e612c4aef25ef787b02a78f5
Fatih Deniz <deniz.fatih@hotmail.de>
2022-12-31 06:14:12 +0100

    Small Study Group (SSG)

---
+++ src/main/java/ssg/ssg12nestedloop/NestedLoop04.java
@@ -0,0 +1,32 @@
+package ssg.ssg12nestedloop;
+
+public class NestedLoop04 {
+    public static void main(String[] args) {
+
+
+         /*
+        * * * * .
+        * * * . .
+        * * . . .
+        * . . . .
+        . . . . .
+         */
+
+
+        int input1 = 5;
+        for (int line = 1; line <= input1; line++) {
+
+            for (int j = 1; j <= (input1 - line); j++) {
+                System.out.print("*" + " ");
+            }
+
+            for (int k = 1; k <= line; k++) {
+                System.out.print("." + " ");
+            }
+
+            System.out.println();
+
+        }
+
+    }
+}
