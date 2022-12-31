eff5cec779bcb84f34c10f6932f26a01fd35a741
Fatih Deniz <deniz.fatih@hotmail.de>
2022-12-31 05:40:32 +0100

    Small Study Group (SSG)

---
+++ src/main/java/ssg/ssg02methodCreation/Calcualator.java
@@ -0,0 +1,18 @@
+package ssg.ssg02methodCreation;
+
+public class Calcualator {
+    // iki variable'in degerlerini Cikarma yapan  bir method olusturalim
+    public static void Cikarma(int a, int b) {
+        System.out.println(a + b);
+    }
+
+    // iki variable'in degerlerini Bolma yapan  bir method olusturalim
+    public static void Bolme(int a, int b) {
+        System.out.println(a / b);
+    }
+
+    // iki variable'in degerlerini Carpimi yapan  bir method olusturalim
+    public static void Carpimi(int a, int b) {
+        System.out.println(a * b);
+    }
+}
