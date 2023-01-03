d71b2619f6b5097114f46d5a9ca1c14214b2d688
Fatih Deniz <deniz.fatih@hotmail.de>
2023-01-03 01:19:40 +0100

    Small Study Group(SSG)

---
+++ src/main/java/ssg/ssg13whileloopdowhileloop/DoWhileLoop01.java
@@ -0,0 +1,29 @@
+package ssg.ssg13whileloopdowhileloop;
+
+public class DoWhileLoop01 {
+    public static void main(String[] args) {
+
+        // kullanicidan pozitif bir tamsayi alip
+        //do  while loop ile sayidan kucuk pozitif tamsayilari yazdiralim
+        int input = 5;
+
+        // Ayni soruyu do-while loop ile yapalim
+        int sayi = 1;
+        do {
+            System.out.println(sayi);
+            sayi++;
+        } while (sayi < input);
+    }
+
+        /*
+            int sayi=1;
+
+            while (sayi<input){
+                System.out.println(sayi);
+                sayi++;
+            }
+
+             */
+
+
+}
