b13d40f1d424f557efd3afa77c5a3ef447df6541
Fatih Deniz <deniz.fatih@hotmail.de>
2023-01-03 01:25:12 +0100

    Small Study Group(SSG)

---
+++ src/main/java/ssg/ssg13whileloopdowhileloop/WhileLoop02.java
@@ -0,0 +1,29 @@
+package ssg.ssg13whileloopdowhileloop;
+
+public class WhileLoop02 {
+    public static void main(String[] args) {
+
+        // kullanicidan iki tamsayi alip
+        // bu sayilari ve aralarindaki tum tamsayilari yazdiran bir kod olusturun
+
+        // ayni soruyu while loop ile yapalim
+        int baslangic = 40;
+        int bitis = 60;
+        while (baslangic <= bitis) {
+
+            System.out.print(baslangic + " ");
+            baslangic++;
+        }
+        System.out.println("");
+
+        /*
+           int baslangic=40;
+        int bitis=60;
+          for (int i = baslangic; i <=bitis ; i++) {
+          System.out.print(i + " ");
+        }
+        System.out.println("");
+         */
+
+    }
+}
