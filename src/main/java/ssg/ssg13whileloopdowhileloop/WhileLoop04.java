78e677547ddce046f2a051b5af9803f9998387db
Fatih Deniz <deniz.fatih@hotmail.de>
2023-01-03 01:29:42 +0100

    Small Study Group(SSG)

---
+++ src/main/java/ssg/ssg13whileloopdowhileloop/WhileLoop04.java
@@ -0,0 +1,41 @@
+package ssg.ssg13whileloopdowhileloop;
+
+import java.util.Scanner;
+
+public class WhileLoop04 {
+    public static void main(String[] args) {
+
+        // kullanicidan toplamak istedigi sayilari alin
+        // ve kullanici 0'a basincaya kadar devam edin
+        // kullanici 0'a bastiginda
+        // girdigi tum sayilarin toplamini yazdirin
+
+        Scanner input = new Scanner(System.in);
+
+        int sayi = 1;
+        int toplam = 0;
+        int sayac = 1;
+        /*
+        while (sayi!=0){
+            System.out.println("lutfen bir sayi Giriniz");
+            sayi=input.nextInt();
+            toplam+=sayi;
+            sayac++;
+        }
+        System.out.println( (sayac-1)   + "  sayi girdiniz  ve  Topplmai : "+ toplam);
+
+
+         */
+
+        System.out.println();
+
+        do {
+            System.out.println("lutfen bir sayi Giriniz");
+            sayi = input.nextInt();
+            toplam += sayi;
+            sayac++;
+
+        } while (sayi != 0);
+        System.out.println((sayac - 1) + "  sayi girdiniz  ve  Topplmai : " + toplam);
+    }
+}
