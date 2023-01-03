ee47e4869d43bba0ef91a3c2f51695d1dd65c67a
Fatih Deniz <deniz.fatih@hotmail.de>
2023-01-03 01:22:27 +0100

    Small Study Group(SSG)

---
+++ src/main/java/ssg/ssg13whileloopdowhileloop/ForWhileDoWhileLoop.java
@@ -0,0 +1,31 @@
+package ssg.ssg13whileloopdowhileloop;
+
+public class ForWhileDoWhileLoop {
+    public static void main(String[] args) {
+
+        //Soru 1) 5 ile 10 arasindaki(5 ve  10 dahil) sayilari  ayni satirda yazdirin
+
+        // 1 . yol
+        for (int i = 1; i <= 5; i++) {
+            System.out.print(i + " ");
+
+        }
+        System.out.println();
+        // 2.yol
+        int k = 1;
+        while (k <= 5) {
+            System.out.print(k + " "); // zero execution
+            k++;
+        }
+
+        System.out.println();
+        // arkadas secme oyunu
+        //3 .yol
+        int j = 1;
+        do {
+            System.out.print(j + " ");// zero yok en az bir kare calisir .
+            j++;
+
+        } while (j <= 5);
+    }
+}
