ee191360e185bc422d8d2374ff3e15b5738a742d
Fatih Deniz <deniz.fatih@hotmail.de>
2023-01-05 23:01:29 +0100

    Regular Practice

---
+++ src/main/java/practice/regular_practice/nighttime06/MethodReturn.java
@@ -0,0 +1,14 @@
+package practice.regular_practice.nighttime06;
+
+public class MethodReturn {
+
+    public static void main(String[] args) {
+
+        System.out.println(dikdortgeninAlani(5, 10)+9);
+
+    }
+
+    private static int dikdortgeninAlani(int a, int b) {
+        return a*b;
+    }
+}
