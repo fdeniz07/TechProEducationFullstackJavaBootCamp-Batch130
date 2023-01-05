93274cc276e7fedc631c441aab2fb301ff91c7c1
Fatih Deniz <deniz.fatih@hotmail.de>
2023-01-05 23:02:09 +0100

    Regular Practice

---
+++ src/main/java/practice/regular_practice/nighttime06/MethodVoid.java
@@ -0,0 +1,18 @@
+package practice.regular_practice.nighttime06;
+
+public class MethodVoid {
+
+    //void ile return arasindaki farklar
+    public static void main(String[] args) {
+
+        //Dikdortgenin Alanini hesapplayan bir kod yaziniz
+
+        dikdortgeninAlani(5,10);
+
+    }
+
+    private static void dikdortgeninAlani(int a, int b) {
+
+        System.out.println(a * b);//50
+    }
+}
