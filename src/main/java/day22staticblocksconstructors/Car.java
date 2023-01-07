7b8bc6eb08c845c18bd58f859d60a49e803e0981
Fatih Deniz <deniz.fatih@hotmail.de>
2023-01-07 20:06:47 +0100

    Day 22

---
+++ src/main/java/day22staticblocksconstructors/Car.java
@@ -0,0 +1,21 @@
+package day22staticblocksconstructors;
+
+    /*
+        Constructor nedir?
+        Class'Dan object üretmemize yarayan code block'laridir.
+
+        Class olusturdugumuzda JAva bize otomatik olarak bir condtructor verir. Ama bu construtor gözle  görülmez, gözle görülmeyen otomatik olak Java tarafindan
+        verilen bu constructor'lara "defaut constructor" denir
+
+        "default constructor'lar "Car(){}" seklindedir.
+     */
+
+
+public class Car {
+    String make = "Honda";
+    String model = "Accord";
+    int year = 2023;
+    boolean hybrid = true;
+}
+
+
