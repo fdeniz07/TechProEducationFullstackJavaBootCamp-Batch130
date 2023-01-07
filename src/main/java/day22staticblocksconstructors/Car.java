70e4a85390e54070baba5ed824f25d345d42bc0f
Fatih Deniz <deniz.fatih@hotmail.de>
2023-01-07 21:02:45 +0100

    Day 22

--- src/main/java/day22staticblocksconstructors/Car.java
+++ src/main/java/day22staticblocksconstructors/Car.java
@@ -8,6 +8,19 @@ package day22staticblocksconstructors;
         verilen bu constructor'lara "defaut constructor" denir
 
         "default constructor'lar "Car(){}" seklindedir.
+
+        Siz kendi constructor'inizi olusturdugunuzda Java default constructor'i siler.
+
+        Bir Class'da farkli parametreler kullanarak istediginiz kadar Constructor olusturabilirsiniz.
+
+        Farkli constractor'lar sayesinde bir class'dan farkli farkli object'ler olusturabiliriz.
+
+        Interview Sorusu: Method ile Constructor'un farki var midir? Varsa nedir?
+        Cevap : Method ile Constructor farkli yapilardir.
+                i) Methodlarda return type vardir ama constructor'larda return type yoktur
+                ii) Method'larin isimleri yaptiklari ise göre developerlar tarafindan belirlenir.
+                    Constructor'larin isimleri ise her zaman Class ismi ile ayni olmalidir.
+
      */
 
 
@@ -16,6 +29,41 @@ public class Car {
     String model = "Accord";
     int year = 2023;
     boolean hybrid = true;
+
+    Car(String make, String model, int year, boolean hybrid) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.hybrid = hybrid;
+    }
+
+    public Car(String make, String model) {
+        this.make = make;
+        this.model = model;
+
+        if (year == 2023) {
+            this.year = 0;
+
+        }
+        if (hybrid == true) {
+            this.hybrid = false;
+        }
+    }
+
+    public Car(String make, int year) {
+        this.make = make;
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "make='" + make + '\'' +
+                ", model='" + model + '\'' +
+                ", year=" + year +
+                ", hybrid=" + hybrid +
+                '}';
+    }
 }
 
 
