65babf07b2edc35a828292a666b81732cb85650d
Fatih Deniz <deniz.fatih@hotmail.de>
2023-01-09 17:31:39 +0100

    SSG 13

---
+++ src/main/java/ssg/ssg13arrays/C01_arrays.java
@@ -0,0 +1,14 @@
+package ssg.ssg13arrays;
+
+import java.util.Arrays;
+
+public class C01_arrays {
+    public static void main(String[] args) {
+        /*
+        1.soru bir array olusturun ve olusturdugunuz arraydeki tum elemanları yazdırın
+        */
+        int arr[] = {1, 2, 3};
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
