fc0cdafa013931fe4f9a5547047195a3ff2243b6
Fatih Deniz <deniz.fatih@hotmail.de>
2023-01-10 09:33:46 +0100

    Advanced Practice 06

---
+++ src/main/java/practice/advanced_practice/practice06/Hacim.java
@@ -0,0 +1,15 @@
+package practice.advanced_practice.practice06;
+
+public class Hacim {
+    public int hacimHesapla(int a){
+        return a*a*a;
+    }
+
+    public int hacimHesapla(int a, int b){
+        return a*a*b;
+    }
+
+    public int hacimHesapla(int a, int b, int c){
+        return a*b*c;
+    }
+}
