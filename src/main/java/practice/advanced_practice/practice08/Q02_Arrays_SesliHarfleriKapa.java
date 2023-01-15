121ba014bf5bc609983638c37642de9c7aa882b7
Fatih Deniz <deniz.fatih@hotmail.de>
2023-01-15 22:24:37 +0100

    Advanced Practice

---
+++ src/main/java/practice/advanced_practice/practice08/Q02_Arrays_SesliHarfleriKapa.java
@@ -0,0 +1,36 @@
+package practice.advanced_practice.practice08;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Q02_Arrays_SesliHarfleriKapa {
+
+    /*Kullanıcıdan alınan bir kelimedeki sesli harfleri kapatıp her bir karakteri bir array elemanı olarak yazdıran bir kod yazınız.
+    Örn:
+    input: Merhaba
+    output: [M, *, r, h, *, b, *]
+*/
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Bir kelime giriniz");
+        String str = input.nextLine();
+
+        String[] arr = new String[str.length()];
+        int idx = 0;
+
+        for (String w : str.split("")) {
+
+            if (w.equalsIgnoreCase("a") || w.equalsIgnoreCase("e") || w.equalsIgnoreCase("i") || w.equalsIgnoreCase("o") || w.equalsIgnoreCase("u")) {
+                arr[idx] = "*";
+                idx++;
+            } else {
+                arr[idx] = w;
+                idx++;
+            }
+        }
+
+        System.out.println(Arrays.toString(arr));
+
+    }
+}
