4e285b797be221b347ec4eac646bf84b6d18245b
Fatih Deniz <deniz.fatih@hotmail.de>
2023-01-15 22:26:01 +0100

    Advanced Practice

---
+++ src/main/java/practice/advanced_practice/practice08/Q03_ObjeOlusturma_Kutuphane.java
@@ -0,0 +1,14 @@
+package practice.advanced_practice.practice08;
+
+public class Q03_ObjeOlusturma_Kutuphane {
+
+    //Kütüphaneye eklemek istediğiniz kitapları, adı, yazar adı, sayfa sayısı, seri numarası ile yazdıran bir kod yazınız.
+
+    public static void main(String[] args) {
+        Kutuphane obj1 = new Kutuphane("Mağaradakiler", "Cemil Meriç", 285);
+        obj1.kitapBilgileri();
+        Kutuphane obj2 = new Kutuphane("Kırlangıç Çığlığı", "Ahmet Ümit", 400);
+        obj2.sayfaSayisi = 405;
+        obj2.kitapBilgileri();
+    }
+}
