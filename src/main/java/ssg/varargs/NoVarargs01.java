8d5447a6b3db8d1b454827734ca006973e9aedde
Fatih Deniz <deniz.fatih@hotmail.de>
2023-01-17 08:54:52 +0100

    SSG

---
+++ src/main/java/ssg/varargs/NoVarargs01.java
@@ -0,0 +1,27 @@
+package ssg.varargs;
+
+public class NoVarargs01 {
+    public static void main(String[] args) {
+
+        // iki sayinin toplyan bir method olustrun .
+
+        dortSayiTopla(3, 5, 6, 6);// 4 Arguments
+        ikiSayiTopla(45, 60);// 2 arguments
+        display();// zero arguments
+
+    }
+
+    public static void ikiSayiTopla(int a, int b) {// 2 paremterli
+
+        System.out.println(a + b);
+    }
+
+    public static void display() {// zero paramter
+        System.out.println();
+    }
+
+    public static void dortSayiTopla(int a, int b, int c, int d) {// 4 paremterli
+
+        System.out.println(a + b);
+    }
+}
