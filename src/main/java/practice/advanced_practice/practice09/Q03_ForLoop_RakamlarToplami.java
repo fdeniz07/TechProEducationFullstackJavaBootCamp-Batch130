acea11205b76874006e6c6dba06012d8aa71ffd2
Fatih Deniz <deniz.fatih@hotmail.de>
2023-01-17 10:30:36 +0100

    Advanced Practice

---
+++ src/main/java/practice/advanced_practice/practice09/Q03_ForLoop_RakamlarToplami.java
@@ -0,0 +1,40 @@
+package practice.advanced_practice.practice09;
+
+import java.util.Scanner;
+
+public class Q03_ForLoop_RakamlarToplami {
+
+    /*
+         Kullanıcıdan alınan bir String içerisindeki rakamların toplamını hesaplayan bir method yazınız.
+         Örn:
+         input : J4\/4 1$ 34$¥
+         output : 16
+         İpucu:
+         Character.isDigit()
+         Integer.valueOf()
+    */
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Bir deger giriniz");
+        String deger = input.nextLine();
+
+        System.out.println(rakmalarToplami(deger));
+    }
+
+    static double rakmalarToplami(String str) {
+        int toplam = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+
+            if (Character.isDigit(str.charAt(i))) {
+                toplam += Integer.valueOf("" + str.charAt(i)); //Char i önce stringe daha sonrada Integer e ceviriyoruz
+            }
+        }
+        System.out.println("toplam = " + toplam);
+
+        return toplam;
+    }
+
+}
