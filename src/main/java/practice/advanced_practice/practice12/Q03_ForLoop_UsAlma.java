7aa7caff7a514b702b9586c8d48aa800daea9644
Fatih Deniz <deniz.fatih@hotmail.de>
2023-01-28 18:22:29 +0100

    Advanced Practice

--- src/main/java/practice/advanced_practice/practice12/Q03_ForLoop_UsAlma.java
+++ src/main/java/practice/advanced_practice/practice12/Q03_ForLoop_UsAlma.java
@@ -5,6 +5,27 @@ public class Q03_ForLoop_UsAlma {
     //Bir sayının istenen kuvvetini hesaplayan bir method yazınız.
 
     public static void main(String[] args) {
+        System.out.println(kuvvetiniAl(5, 2));
 
+    }
+
+    public static double kuvvetiniAl(double taban, int us) {
+        double sonuc = 1;
+
+        if (taban == 0 && us == 0) {
+            throw new ArithmeticException("Taban ve us ayni anda 0 olamaz");
+        }
+
+        if (us >= 0) {
+            for (int i = 0; i < us; i++) {
+                sonuc *= taban;
+            }
+            return sonuc;
+        } else {
+            for (int i = 0; i < Math.abs(us); i++) {
+                sonuc *= taban;
+            }
+            return 1 / sonuc;
+        }
     }
 }
