fda57a19d091b174d4c329b2404d6f2b1f96311c
Fatih Deniz <deniz.fatih@hotmail.de>
2023-01-28 17:19:37 +0100

    Advanced Practice

---
+++ src/main/java/practice/advanced_practice/practice12/Q03_ForLoop_UsAlma.java
@@ -0,0 +1,10 @@
+package practice.advanced_practice.practice12;
+
+public class Q03_ForLoop_UsAlma {
+
+    //Bir sayının istenen kuvvetini hesaplayan bir method yazınız.
+
+    public static void main(String[] args) {
+
+    }
+}
