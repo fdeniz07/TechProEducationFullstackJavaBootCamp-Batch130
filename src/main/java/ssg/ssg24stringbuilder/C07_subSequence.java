4dfadef17a83c233082d25ec946b0c878807af82
Fatih Deniz <deniz.fatih@hotmail.de>
2023-02-13 21:06:54 +0100

    SSG

---
+++ src/main/java/ssg/ssg24stringbuilder/C07_subSequence.java
@@ -0,0 +1,11 @@
+package ssg.ssg24stringbuilder;
+
+public class C07_subSequence {
+    public static void main(String[] args) {
+        StringBuilder sb1 = new StringBuilder("Java cok guzeldir");
+        sb1.subSequence(5, 8);//
+        sb1.substring(5);//
+        System.out.println(sb1);
+
+    }
+}
