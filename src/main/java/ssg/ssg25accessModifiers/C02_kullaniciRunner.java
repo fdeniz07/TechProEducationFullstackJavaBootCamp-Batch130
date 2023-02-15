9f47ba74cee50d49e8fb8d6c8996bc5fef65f227
Fatih Deniz <deniz.fatih@hotmail.de>
2023-02-15 19:46:41 +0100

    SSG

---
+++ src/main/java/ssg/ssg25accessModifiers/C02_kullaniciRunner.java
@@ -0,0 +1,12 @@
+package ssg.ssg25accessModifiers;
+
+public class C02_kullaniciRunner {
+    public static void main(String[] args) {
+
+        C01_kullanici k1=new C01_kullanici();
+
+        System.out.println(k1.kullaniciAdi);
+        System.out.println(k1.kullaniciId);
+        System.out.println(k1.ePosta);
+    }
+}
