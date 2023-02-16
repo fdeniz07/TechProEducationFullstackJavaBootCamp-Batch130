94403e89fb046dc71c844ee7f9de147166bfcced
Fatih Deniz <deniz.fatih@hotmail.de>
2023-02-16 19:38:00 +0100

    SSG

---
+++ src/main/java/ssg/ssg26encapsulation/TasitRunner.java
@@ -0,0 +1,11 @@
+package ssg.ssg26encapsulation;
+
+public class TasitRunner {
+
+    public static void main(String[] args) {
+        Tasit tst1 = new Tasit();
+        tst1.setTasitTuru("Tir");
+
+        System.out.println(tst1.getTasitTuru());
+    }
+}
