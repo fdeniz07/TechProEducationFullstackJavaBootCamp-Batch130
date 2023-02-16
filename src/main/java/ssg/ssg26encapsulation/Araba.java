94ddaddc779ef24631eae80be3249a2855b4c42c
Fatih Deniz <deniz.fatih@hotmail.de>
2023-02-16 19:35:44 +0100

    SSG

---
+++ src/main/java/ssg/ssg26encapsulation/Araba.java
@@ -0,0 +1,17 @@
+package ssg.ssg26encapsulation;
+
+public class Araba {
+
+    String marka="Marka belirtilmedi";
+    private String model="Model belirtilmedi";
+
+    public String getYakit() {
+        return yakit;
+    }
+
+    private String yakit="Dizel";
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+}
