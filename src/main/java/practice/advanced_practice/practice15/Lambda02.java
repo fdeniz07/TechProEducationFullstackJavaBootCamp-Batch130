9d5ce8589c99bab73b1b4012e40804cf82ca6b72
Fatih Deniz <deniz.fatih@hotmail.de>
2023-02-18 22:05:34 +0100

    Advantage Practice

--- src/main/java/practice/advanced_practice/practice15/Lambda02.java
+++ src/main/java/practice/advanced_practice/practice15/Lambda02.java
@@ -7,7 +7,7 @@ public class Lambda02 {
 
         System.out.println(birdenXeKadarDegerlerToplami(5));
         System.out.println(birdenXeKadarTekSayilarinToplami(10));
-
+        System.out.println(birdenSonsuzaKadarGidenTekSayilardanIlkXTanesiniTopla(6));
     }
 
     //SORU1: 1'den x'e kadar int degerleri akısa alınız(x dahil olacak), toplamını bulup return ederek yazdırınız.
@@ -21,8 +21,17 @@ public class Lambda02 {
     //SORU2: 1'den x'e kadar int degerleri akısa alınız(x dahil olacak), tek sayıların, toplamını bulup return ederek yazdırınız. (x dahil olacak)
     public static int birdenXeKadarTekSayilarinToplami(int x) {
 
-        int sonuc = IntStream.rangeClosed(1, x).filter(t->t%2==1).sum();
+        int sonuc = IntStream.rangeClosed(1, x).filter(t -> t % 2 != 0).sum();
         return sonuc;
+
+    }
+
+    //SORU3: 1'den sonsuza kadar giden Tek sayılardan, ilk x tanesini toplayınız sonucu return ederek yazdırınız
+    public static int birdenSonsuzaKadarGidenTekSayilardanIlkXTanesiniTopla(int x){
+
+    int sonuc = IntStream.iterate(1,t->t+2).limit(x).sum()  ;
+
+    return sonuc;
 
     }
 }
