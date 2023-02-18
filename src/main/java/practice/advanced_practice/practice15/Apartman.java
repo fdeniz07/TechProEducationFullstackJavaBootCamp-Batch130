0dd85373f6f53479abe4fe4a5f3194ce2cf91756
Fatih Deniz <deniz.fatih@hotmail.de>
2023-02-18 19:38:56 +0100

    Advantage Practice

---
+++ src/main/java/practice/advanced_practice/practice15/Apartman.java
@@ -0,0 +1,35 @@
+package practice.advanced_practice.practice15;
+
+public class Apartman {
+
+    private String cephe;
+    private int katSayi;
+    private int kira;
+
+    public Apartman() {
+    }
+
+    public String getCephe() {
+        return cephe;
+    }
+
+    public void setCephe(String cephe) {
+        this.cephe = cephe;
+    }
+
+    public int getKatSayi() {
+        return katSayi;
+    }
+
+    public void setKatSayi(int katSayi) {
+        this.katSayi = katSayi;
+    }
+
+    public int getKira() {
+        return kira;
+    }
+
+    public void setKira(int kira) {
+        this.kira = kira;
+    }
+}
