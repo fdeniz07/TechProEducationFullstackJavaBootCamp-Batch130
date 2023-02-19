8c8cea8abe1a7e6ac234d4fbb6e402aabb9ca093
Fatih Deniz <deniz.fatih@hotmail.de>
2023-02-19 21:34:35 +0100

    SSG

---
+++ src/main/java/ssg/ssg27inheritance/C04_cons1.java
@@ -0,0 +1,11 @@
+package ssg.ssg27inheritance;
+
+public class C04_cons1 {
+    String str="lale";
+    C04_cons1(){
+        System.out.println("C04teki cons calisti");
+
+    }
+    void ekleme(){
+    }
+}
