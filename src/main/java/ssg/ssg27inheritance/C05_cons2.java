f721aab404e3a8aad3c46d18e2424ebffd302e1d
Fatih Deniz <deniz.fatih@hotmail.de>
2023-02-19 21:35:02 +0100

    SSG

---
+++ src/main/java/ssg/ssg27inheritance/C05_cons2.java
@@ -0,0 +1,15 @@
+package ssg.ssg27inheritance;
+
+public class C05_cons2 extends C04_cons1 {
+
+    String str="zambak";
+    C05_cons2(){
+        System.out.println("C05teki cons calisti");
+
+    }
+
+    @Override
+    void ekleme() {
+
+    }
+}
